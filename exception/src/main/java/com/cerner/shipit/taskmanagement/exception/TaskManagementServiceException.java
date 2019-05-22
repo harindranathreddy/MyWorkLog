21c18fcb40010b3e07f31bc0c7baaa02dc0ffd8d
HarindranathReddy,KJ <HarindranathReddy.KJ@cerner.com>
2019-05-22 17:57:15 +0530

    Exception Class

---
+++ exception/src/main/java/com/cerner/shipit/taskmanagement/exception/TaskManagementServiceException.java
@@ -0,0 +1,32 @@
+package com.cerner.shipit.taskmanagement.exception;
+
+public class TaskManagementServiceException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private final String errorCode;
+	private final String errorMessage;
+	
+	 public TaskManagementServiceException(String errorCode, String errorMessage) {
+	        super();
+	        this.errorCode = errorCode;
+	        this.errorMessage = errorMessage;
+	    }
+
+	    public TaskManagementServiceException(Throwable cause, String errorCode, String errorMessage) {
+	        super(cause);
+	        this.errorCode = errorCode;
+	        this.errorMessage = errorMessage;
+	    }
+
+	    public String getErrorCode() {
+	        return this.errorCode;
+	    }
+
+	    public String getErrorMessage() {
+	        return this.errorMessage;
+	    }
+
+}
